7dfe92bf5776f2952cae9f8e1a5e9b2fc865cdb2
Janet Huacahuasi <janet.huacahuasi.garcia@orbis.com.pe>
2019-11-29 10:49:29 -0500

    chore: se agrega next-images para importar imagenes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next';
+import bg from './../static/example-mail.png';
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - user agent: {userAgent}</h1>
+  <>
+    <h1>Hello world! - user agent: {userAgent}</h1>
+    <img src={bg} alt="mail"/>
+  </>
 );
 
 Home.getInitialProps = async ({ req }) => {
